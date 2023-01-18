a6e0fa0551b6e8542357ed034580825de0c82f4e
Allen Ding <allen.ding@hotmail.com>
2023-01-18 12:41:04 -0800

    return text file info

---
+++ syncBreeds.js
@@ -0,0 +1,9 @@
+// synchronous function to fetch a cat breed
+const breedDetails = function(breed) {
+  // can simply return it (easy peezee, butter squeezy) ...
+  return catBreeds[breed];
+};
+
+// get the return value right away from the function
+const bombay = breedDetails('Bombay');
+console.log(bombay); //=> prints out the description for that breed
